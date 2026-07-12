531510dc48d1298414d8d7d775574707711da8bb
rajabhu2017 <rajabhu2017@gmail.com>
2026-07-12 18:37:07 +0530

    Add files via upload

---
+++ simple_chain.py
@@ -0,0 +1,25 @@
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_core.prompts import load_prompt
+from dotenv import load_dotenv
+from langchain_core.prompts import PromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+load_dotenv()
+
+prompt = PromptTemplate(
+    template='Generate 5 interesting facts about {topic}',
+    input_variables=['topic']
+)
+# Load Gemini model
+model = ChatGoogleGenerativeAI(
+    model="gemini-2.5-flash"
+)
+
+parser = StrOutputParser()
+
+chain = prompt | model | parser
+
+result = chain.invoke({'topic':'cricket'})
+
+print(result)
+
+chain.get_graph().print_ascii()
